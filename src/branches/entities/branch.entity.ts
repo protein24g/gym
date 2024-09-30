658a5ffe7c49e92810819690f28c06f2b64aa493
protein <dns6574@naver.com>
2024-09-30 14:15:56 +0900

    들여쓰기 수정

--- src/branches/entities/branch.entity.ts
+++ src/branches/entities/branch.entity.ts
@@ -3,25 +3,25 @@ import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "t
 
 @Entity()
 export class Branch {
-    @PrimaryGeneratedColumn()
-    id: number;
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column({type: "varchar", length: 50, nullable: false})
-    name: string;
+  @Column({type: "varchar", length: 50, nullable: false})
+  name: string;
 
-    @Column({type: 'text', nullable: false})
-    address: string;
+  @Column({type: 'text', nullable: false})
+  address: string;
 
-    @Column({type: 'text', nullable: false})
-    addressDetail: string;
+  @Column({type: 'text', nullable: false})
+  addressDetail: string;
 
-    @Column({type: 'varchar', length: 20, nullable: false})
-    phone: string;
+  @Column({type: 'varchar', length: 20, nullable: false})
+  phone: string;
 
-    @Column({type: 'text', nullable: false})
-    openingHours: string;
+  @Column({type: 'text', nullable: false})
+  openingHours: string;
 
-    @ManyToOne(() => Gym, gym => gym.branches)
-    @JoinColumn({name: 'gymId'})
-    gym: Gym;
+  @ManyToOne(() => Gym, gym => gym.branches)
+  @JoinColumn({name: 'gymId'})
+  gym: Gym;
 }
--- src/gym/entities/gym.entity.ts
+++ src/gym/entities/gym.entity.ts
@@ -3,15 +3,15 @@ import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
 export class Gym {
-    @PrimaryGeneratedColumn()
-    id: number;
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column({type: "varchar", length: 50, nullable: false})
-    name: string;
+  @Column({type: "varchar", length: 50, nullable: false})
+  name: string;
 
-    @Column({type: 'text'})
-    description: string;
+  @Column({type: 'text'})
+  description: string;
 
-    @OneToMany(() => Branch, branch => branch.gym)
-    branches: Branch[];
+  @OneToMany(() => Branch, branch => branch.gym)
+  branches: Branch[];
 }
