8c9a6b90ea6357b1f3c690dc762bd72bf609aead
kmcu <ds@kmcu.ac.kr>
2024-09-30 12:49:48 +0900

    typeorm 모듈 설정, 엔티티(gym, branch) 생성

---
+++ src/gym/entities/gym.entity.ts
@@ -0,0 +1,17 @@
+import { Branch } from "src/branches/entities/branch.entity";
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Gym {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({type: "varchar", length: 50, nullable: false})
+    name: string;
+
+    @Column({type: 'text'})
+    description: string;
+
+    @OneToMany(() => Branch, branch => branch.gym)
+    branches: Branch[];
+}
