a3a859469f44714fcb16f70e8ea29930a890db84
protein <dns6574@naver.com>
2024-11-07 22:30:00 +0900

    대시보드, Box 컴포넌트 수정

--- front/src/components/ui/Box.tsx
+++ front/src/components/ui/Box.tsx
@@ -1,11 +1,23 @@
 import { FC } from "react"
 
-const Box: FC<{icon: React.ReactNode, num: string}> = ({icon, num}) => {
+const Box: FC<{icon: React.ReactNode, num: string, describe: string}> = ({icon, num, describe}) => {
   return (
-    <div className="flex items-center justify-between h-24 px-8 rounded-lg border bg-gray-800">
-      <div>{icon}</div>
-      <div className="text-xl">{num}</div>
+    <div className="mb-0 flex flex-col justify-between m-4 p-4 bg-white rounded-lg border">
+      <div className="flex justify-between">
+        <p className="text-gray-400 font-bold">{describe}</p>
+        <div>{icon}</div>
+      </div>
+      <div>
+        <p className="text-black text-2xl font-bold">{num}</p>
+      </div>
     </div>
+    // <div className="flex m-4 p-4 bg-white rounded-lg border text-sm gap-4">
+    //   <div>{icon}</div>
+    //   <div className="flex flex-col gap-1">
+    //     <p className="text-black text-xl font-bold">{num}</p>
+    //     <p className="text-gray-400 font-bold">{describe}</p>
+    //   </div>
+    // </div>
   )
 }
 
--- front/src/pages/sidebar/DashboardPage.tsx
+++ front/src/pages/sidebar/DashboardPage.tsx
@@ -1,18 +1,16 @@
 import { FC } from "react"
 import Box from "../../components/ui/Box"
-import { FaCalendarCheck, FaUsers } from "react-icons/fa"
-import { SiTrainerroad } from "react-icons/si"
-import { MdCancel } from "react-icons/md"
+import { FaCheck, FaUsers } from "react-icons/fa"
+import { PiNetwork } from "react-icons/pi"
+import { RiUserSettingsLine } from "react-icons/ri"
 
 const DashboardPage: FC = () => {
   return (
-    <div className="m-4 p-4 text-sm">
-      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-6 lg:grid-cols-4 2xl:gap-7.5 text-white">
-        <Box icon={<FaUsers className="w-8 h-8"/>} num={'661'}/>
-        <Box icon={<FaCalendarCheck className="w-8 h-8"/>} num={'34'}/>
-        <Box icon={<SiTrainerroad  className="w-8 h-8"/>} num={'6'}/>
-        <Box icon={<MdCancel className="w-8 h-8"/>} num={'16'}/>
-      </div>
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 text-white">
+      <Box icon={<PiNetwork className="w-12 h-12 p-2 bg-gray-500 rounded-full"/>} num={'1,326'} describe={'지점'}/>
+      <Box icon={<FaUsers className="w-12 h-12 p-2 bg-blue-500 rounded-full"/>} num={'326'} describe={'전체 회원'}/>
+      <Box icon={<RiUserSettingsLine className="w-12 h-12 p-2 bg-blue-500 rounded-full"/>} num={'7'} describe={'트레이너'}/>
+      <Box icon={<FaCheck className="w-12 h-12 p-2 bg-green-500 rounded-full"/>} num={'66'} describe={'출석'}/>
     </div>
   )
 }
